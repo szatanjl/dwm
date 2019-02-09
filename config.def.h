a0dc8fb6aa6300464439d0679c6495b760c34118
szatanjl <szatan@gecc.xyz>
2019-02-09 13:51:32 +0100

    Maximize floating windows
    Add two functions:
        - maximize() maximizes focused floating window
        - zoom_or_maximize() zooms focused window if tiled or maximizes it
          if floating

--- config.def.h
+++ config.def.h
@@ -81,7 +81,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Return, zoom_or_maximize, {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
