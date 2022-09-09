9b2bb9b8b95ae6fb8d4a0799d1f0ea25f9064e74
szatanjl <szatan@gecc.xyz>
2022-09-09 10:32:39 +0200

    Configure dwm: Stupid zoom...

--- config.h
+++ config.h
@@ -37,7 +37,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ NULL,       NULL,       NULL,       0,            0,           -1 },
+	{ "zoom",     NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
